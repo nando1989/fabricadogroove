0724ebac3ea31a57a9841e8201a5fa2a1f1ae413
gelaco1989 <gelaco1989@gmail.com>
2025-03-02 22:35:02 -0300

    atualização

---
+++ src/componentes/card/card.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './style.css';
+
+
+
+const Card = () => {
+  return (
+    <div className="container-card">
+     
+      
+
+      
+    </div>
+  );
+};
+
+export default Card;
--- src/visitPage/page.js
+++ src/visitPage/page.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './styles.css';
 import LogoFabrica from '../assets/logo-pulo-do-gato.png';
-import VideoSlider from '../componentes/videoSlider/videoSlider';
+import Harmonia from '../assets/harmonia.png';
 
 
 
@@ -10,24 +10,26 @@ const VisitPage = () => {
   return (
     <>
       <div className="container-method-landing">
-
         <img className="logo-fabrica" src={LogoFabrica} alt="Promoção" />
-
-
         <div class="title">
           <h1>O <cores> pulo do Gato </cores>para estudar em qualquer lugar</h1>
         </div>
-
-        <div className="container-modulos">
-          <div className="modulo-iniciante">
-           
+        <div className="container-cards">
+          <div className='container-cards-in'>
+            <img className="img-harmonia" src={Harmonia} alt="Promoção" />
           </div>
+          <div className='container-cards-in'>
+            <img className="img-harmonia" src={Harmonia} alt="Promoção" />
+          </div>
+          <div className='container-cards-in'>
+            <img className="img-harmonia" src={Harmonia} alt="Promoção" />
+          </div>
+          <div className='container-cards-in'>
+            <img className="img-harmonia" src={Harmonia} alt="Promoção" />
+          </div>
+
         </div>
-
       </div>
-
-
-
     </>
   );
 };
