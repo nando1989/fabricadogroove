a84ca3b31c8721f61d8bd2916a40e4d564219418
gelaco1989 <gelaco1989@gmail.com>
2025-03-03 14:14:30 -0300

    atualização

--- src/visitPage/page.js
+++ src/visitPage/page.js
@@ -1,53 +1,34 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './styles.css';
 import Logo from '../assets/logo.png';
-import Harmonia from '../assets/harmonia.png';
-import Acordes from '../assets/acordes.png';
-import Arpejos from '../assets/arpejos.png';
-import Slap from '../assets/slap.png';
-import { useNavigate } from 'react-router-dom';
-
 
 const VisitPage = () => {
   const navigate = useNavigate();
 
-  const goToHarmonia = () => {
-    navigate('/harmonia');
-  };
-  const goToAcordes = () => {
-    navigate('/acordes');
-  };
-  const goToArpejos = () => {
-    navigate('/Arpejos');
-  };
-  const goToSlap = () => {
-    navigate('/slap');
-  };
   return (
-    <>
-      <div className="container-method-landing">
-        <img className="logo-fabrica" src={Logo} alt="Promoção" />
-        
-        <div class="title">
-          <h1>O <cores> pulo do Gato </cores>para estudar em qualquer lugar</h1>
-        </div>
+    <div className="container-method-landing">
+      <img className="logo-fabrica" src={Logo} alt="Promoção" />
 
-        <div className="container-cards">
-          <div className='container-cards-in'>
-            <img className="img-harmonia" onClick={goToHarmonia} src={Harmonia} alt="Promoção" />
-          </div>
-          <div className='container-cards-in'>
-            <img className="img-harmonia" onClick={goToAcordes} src={Acordes} alt="Promoção" />
-          </div>
-          <div className='container-cards-in'>
-            <img className="img-harmonia" onClick={goToArpejos} src={Arpejos} alt="Promoção" />
-          </div>
-          <div className='container-cards-in'>
-            <img className="img-harmonia" onClick={goToSlap} src={Slap} alt="Promoção" />
-          </div>
+      <div className="title">
+        <h1>O <span className="highlight">pulo do Gato</span> para estudar em qualquer lugar</h1>
+      </div>
+
+      <div className="container-cards">
+        <div className='container-cards-in'>
+          <img className="img-harmonia" onClick={() => navigate('/harmonia')} src="/assets/harmonia.png" alt="Harmonia" />
+        </div>
+        <div className='container-cards-in'>
+          <img className="img-harmonia" onClick={() => navigate('/acordes')} src="/assets/acordes.png" alt="Acordes" />
+        </div>
+        <div className='container-cards-in'>
+          <img className="img-harmonia" onClick={() => navigate('/arpejos')} src="/assets/arpejos.png" alt="Arpejos" />
+        </div>
+        <div className='container-cards-in'>
+          <img className="img-harmonia" onClick={() => navigate('/slap')} src="/assets/slap.png" alt="Slap" />
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
